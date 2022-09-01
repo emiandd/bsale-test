143bcb3de9504502dc45e3c065c5aa844c53d7f7
emiandd <emilandresaravena@icloud.com>
2022-09-01 19:17:00 -0400

    routes: /categories added

---
+++ api/routes/categories/getAllCategories.routes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const router = Router();
+const getAllCategories = require('../../controllers/getAllCategories.controller.js');
+
+router.get('/categories', async (req, res) => {
+
+	try {
+		return await getAllCategories(res);
+	} catch(e) {
+		console.log({error:e});
+	}
+
+})
+
+module.exports = router;
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 const products = require('./products/getAllProducts.routes.js');
+const categories = require('./categories/getAllCategories.routes.js');
 
 router.use('/', products);
-
+router.use('/', categories);
 
 module.exports = router;
