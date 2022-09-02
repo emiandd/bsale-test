d6fb47fc2442147f9c3c07eb326816780eaece2a
emiandd <emilandresaravena@icloud.com>
2022-09-01 22:29:19 -0400

    tests: GET /products/:category added

--- api/__tests__/index.js
+++ api/__tests__/index.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const baseURL = 'http://localhost:3001';
 
-describe('GET /products', () => {
+xdescribe('GET /products', () => {
 
 	it('should return status code 200', async () => {
 		const response = await request(baseURL).get('/products');
@@ -15,7 +15,7 @@ describe('GET /products', () => {
 
 })
 
-describe('GET /categories', () => {
+xdescribe('GET /categories', () => {
 
 	it('should return status code 200', async () => {
 		const response = await request(baseURL).get('/categories');
@@ -27,4 +27,48 @@ describe('GET /categories', () => {
 		expect(response.body).not.toHaveLength(0);
 	})
 
+})
+
+xdescribe('GET /products/:category', () => {
+
+	it('should return status code 200', async () => {
+		const response = await request(baseURL).get(`/products/1`);
+		expect(response.statusCode).toBe(200);
+	})
+
+	it('should return products from the "energetica" category', async () => {
+		const response = await request(baseURL).get(`/products/1`);
+		expect(response.body[0].category).toBe(1)
+
+	})
+
+	it('should return products from the "pisco" category', async () => {
+		const response = await request(baseURL).get(`/products/2`);
+		expect(response.body[0].category).toBe(2)
+	})
+
+	it('should return products from the "ron" category', async () => {
+		const response = await request(baseURL).get(`/products/3`);
+		expect(response.body[0].category).toBe(3)
+	})
+
+	it('should return products from the "bebida" category', async () => {
+		const response = await request(baseURL).get(`/products/4`);
+		expect(response.body[0].category).toBe(4)
+	})
+
+	it('should return products from the "snack" category', async () => {
+		const response = await request(baseURL).get(`/products/5`);
+		expect(response.body[0].category).toBe(5)
+	})
+
+	it('should return products from the "cerveza" category', async () => {
+		const response = await request(baseURL).get(`/products/6`);
+		expect(response.body[0].category).toBe(6)
+	})
+
+	it('should return products from the "vodka" category', async () => {
+		const response = await request(baseURL).get(`/products/7`);
+		expect(response.body[0].category).toBe(7)
+	})
 })
